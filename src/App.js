33e12bd7d30b45a67f3a5f3de77c91202455f068
yoosion030 <rsy5487@naver.com>
2022-09-19 20:57:14 +0900

    Add router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-function App() {
-  return <div className="App">hi</div>;
-}
+import { Route, Routes } from 'react-router-dom';
+import * as P from './pages';
 
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<P.MainPage />} />
+      <Route path="/book/register" element={<P.BookRegisterPage />} />
+    </Routes>
+  );
+};
 export default App;

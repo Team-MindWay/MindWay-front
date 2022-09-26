e77e1a19d29f948fad317b263c04a68fa9f946a0
yoosion030 <rsy5487@naver.com>
2022-09-26 12:59:49 +0900

    Add Title

---
+++ src/components/common/Title/index.js
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+
+const SubTitle = styled.p`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 18px;
+  color: #aad1a0;
+`;
+
+const MainTitle = styled.h1`
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 48px;
+`;
+
+const Title = ({ main, sub }) => {
+  return (
+    <>
+      <SubTitle>{sub}</SubTitle>
+      <MainTitle>{main}</MainTitle>
+    </>
+  );
+};
+
+export default Title;

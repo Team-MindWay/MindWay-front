1809fdd7d16f02766e199dec50697bb9987815ed
yoosion030 <rsy5487@naver.com>
2022-10-02 14:44:25 +0900

    Add MeatballsMenu svg

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -1,3 +1,4 @@
 export { default as Logo } from './Logo';
 export { default as UserIcon } from './UserIcon';
 export { default as Arrow } from './Arrow';
+export { default as MeatballsMenu } from './MeatballsMenu';

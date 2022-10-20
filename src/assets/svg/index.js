c70470373214259d7375a9b465ed93003322b0f8
yoosion030 <rsy5487@naver.com>
2022-10-20 12:29:39 +0900

    Add Icon

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -2,3 +2,5 @@ export { default as Logo } from './Logo';
 export { default as UserIcon } from './UserIcon';
 export { default as Arrow } from './Arrow';
 export { default as MeatballsMenu } from './MeatballsMenu';
+export { default as UpdateIcon } from './UpdateIcon';
+export { default as DeleteIcon } from './DeleteIcon';

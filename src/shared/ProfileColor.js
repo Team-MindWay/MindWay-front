e843f22510baf248917822b914fd7a49dd8bf1c2
yoosion030 <rsy5487@naver.com>
2022-10-21 08:21:33 +0900

    Add ProfileColor

---
+++ src/shared/ProfileColor.js
@@ -0,0 +1,6 @@
+export const profileColor = [
+  '46.72deg, #AAD1A0 14.8%, #F5F5F5 85.49%',
+  '230.87deg, #506C49 14.64%, #F5F5F5 85.89%',
+  '180deg, #7EAF72 0%, #F5F5F5 100%',
+  '0deg, #6E8F66 0%, #F5F5F5 100%',
+];
